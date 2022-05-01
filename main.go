7cffde748f38dfb30c03aed5555471a260cf5b8a
汪心禾 <wangxinhe06@gmail.com>
2022-05-01 15:27:56 +0800

    Simplify AutoSave

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"regexp"
-	"runtime"
-	"runtime/debug"
 	"strconv"
 	"strings"
 	"time"
@@ -17,53 +14,39 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-var timeInterval interface{}
-
-var fetchCount int64 = 0
-
-type AutoSaveFetchStruct struct {
-	Status int `json:"status"`
-	Data   struct {
-		Count  int `json:"Count"`
-		Result []struct {
-			PostID int    `json:"PostID"`
-			Title  string `json:"Title"`
-			Author struct {
-				Instance string `json:"_instance"`
-			} `json:"Author"`
-			Forum struct {
-				ForumID      int    `json:"ForumID"`
-				Name         string `json:"Name"`
-				InternalName string `json:"InternalName"`
-				Instance     string `json:"_instance"`
-			} `json:"Forum"`
-			Top         int  `json:"Top"`
-			SubmitTime  int  `json:"SubmitTime"`
-			IsValid     bool `json:"isValid"`
-			LatestReply struct {
-				Author struct {
-					Instance string `json:"_instance"`
-				} `json:"Author"`
-				ReplyTime int    `json:"ReplyTime"`
-				Content   string `json:"Content"`
-				Instance  string `json:"_instance"`
-			} `json:"LatestReply"`
-			RepliesCount int    `json:"RepliesCount"`
-			Instance     string `json:"_instance"`
-		} `json:"result"`
-	} `json:"data"`
+type LegacyPost struct {
+	PostID int    `json:"PostID"`
+	Title  string `json:"Title"`
+	Author struct {
+		Instance string `json:"_instance"`
+	} `json:"Author"`
+	Forum struct {
+		ForumID      int    `json:"ForumID"`
+		Name         string `json:"Name"`
+		InternalName string `json:"InternalName"`
+		Instance     string `json:"_instance"`
+	} `json:"Forum"`
+	Top         int  `json:"Top"`
+	SubmitTime  int  `json:"SubmitTime"`
+	IsValid     bool `json:"isValid"`
+	LatestReply struct {
+		Author struct {
+			Instance string `json:"_instance"`
+		} `json:"Author"`
+		ReplyTime int    `json:"ReplyTime"`
+		Content   string `json:"Content"`
+		Instance  string `json:"_instance"`
+	} `json:"LatestReply"`
+	RepliesCount int    `json:"RepliesCount"`
+	Instance     string `json:"_instance"`
 }
 
-func JSONToMap(str []byte) AutoSaveFetchStruct {
-	var tempMap AutoSaveFetchStruct
-	err := json.Unmarshal(str, &tempMap)
-
-	if err != nil {
-		fmt.Print("[ERROR] JSON DEAL ERROR WITH AUTOSAVE, LOG:\n", err)
-		return tempMap
-	}
-
-	return tempMap
+type LegacyPostList struct {
+	Status int `json:"status"`
+	Data   struct {
+		Count  int          `json:"count"`
+		Result []LegacyPost `json:"result"`
+	} `json:"data"`
 }
 
 type DBComment struct {
@@ -214,17 +197,10 @@ func ChangeDiscussToDBDiscussTemlate(PostID int) (result DBDiscussTemplate) {
 	return
 }
 
-func SaveNewDiscuss(PostID int) {
+func SaveNewDiscuss(session *mgo.Session, PostID int) {
 	// 检查是否已经存在
-	session, err := mgo.Dial("mongodb://root:rtpwd@localhost:62232")
-	if err != nil {
-		fmt.Print("[Save Warning] can`t connect with mongoDB retrying.. LOG:", err, "\n")
-		SaveNewDiscuss(PostID)
-		return
-	}
 	var discuss DBDiscussTemplate
-	var discussCount int
-	discussCount, err = session.DB("luogulo").C("discuss").Find(bson.M{"postid": PostID}).Count()
+	discussCount, err := session.DB("luogulo").C("discuss").Find(bson.M{"postid": PostID}).Count()
 	if err != nil {
 		fmt.Print("[Save ERROR] Can`t check exist. LOG:", err)
 		return
@@ -285,91 +261,45 @@ func SaveNewDiscuss(PostID int) {
 			fmt.Print("[Save ERROR] ERROR2. LOG:", err, "\n")
 		}
 	}
-	session.Close()
 }
 
-func AutoSave() {
-	runtime.Gosched()
-	debug.SetTraceback("goroutine")
-	fmt.Printf("[Info] AutoSave Tool has been started.\n")
-	//time.Sleep(2 * time.Second)
-	for {
-		// runtime.Gosched()
-		//fmt.Printf("[Info] AutoSave Tool are fetching from Luogu now. \n") 隐藏本条并修改语法。
-		url := "https://www.luogu.com.cn/api/discuss?forum=relevantaffairs&page=1"
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			fmt.Printf("[ERROR] AutoSave Tool can`t get Luogu discuss now. We change the interval to 120 seconds.\n")
-			time.Sleep(120 * time.Second)
-			continue
-		}
-		req.Header.Set("Cookie", "")
-		req.Header.Set("Host", "www.luogu.com.cn")
-		req.Header.Set("Referer", "https://www.luogu.com.cn")
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
-		client := &http.Client{Timeout: time.Second * 15}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Printf("[ERROR] AutoSave Tool can`t get Luogu discuss now. We change the interval to 120 seconds.\n")
-			fmt.Print("[ERROR]Error reading response. ", err, "\n")
-			time.Sleep(120 * time.Second)
-			continue
-		}
-		// defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		dataThings := JSONToMap(body)
-		lenOfDataResult := len(dataThings.Data.Result)
-		fetchCount++
-		for i := 0; i < lenOfDataResult; i++ {
-			var discuss = dataThings.Data.Result[i]
-			if discuss.Top > 2 {
-				// 忽略置顶
-				continue
-			}
-			SaveNewDiscuss(dataThings.Data.Result[i].PostID)
-		}
-		time.Sleep(timeInterval.(time.Duration))
+func AutoSave(session *mgo.Session) {
+	url := "https://www.luogu.com.cn/api/discuss?forum=relevantaffairs&page=1"
+	fmt.Println("Listing", url)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		panic(err)
 	}
-}
+	client := http.Client{Timeout: 15 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	} else if resp.StatusCode != http.StatusOK {
+		panic(resp.Status)
+	}
 
-func SendErrorMessage() {
-	fmt.Print("[FATAL ERROR] ERROR!\n")
+	var result LegacyPostList
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		panic(err)
+	} else if result.Status != http.StatusOK {
+		panic(result.Status)
+	}
+
+	lenOfDataResult := len(result.Data.Result)
+	for i := 0; i < lenOfDataResult; i++ {
+		fmt.Printf("\tSaving %d...\n", result.Data.Result[i].PostID)
+		SaveNewDiscuss(session, result.Data.Result[i].PostID)
+	}
+
+	fmt.Println("Fetched", url)
 }
 
 func main() {
-	timeInterval = 30 * 1000 * time.Millisecond
-	debug.SetTraceback("goroutine")
-	go AutoSave()
-	if false {
-		// 守护
-		defer main()             // 若出现不可以意料的错误
-		defer SendErrorMessage() // 直接报告
-	}
-	for {
-		var command string
-		fmt.Scanln(&command)
-		fmt.Printf("[Info] Deal with " + command + "\n")
-		if command == "changeTime" || command == "ct" {
-			fmt.Printf("[AutoSave] How long do you want to set(millonsecond)?\n")
-			var newTime int64
-			fmt.Scanln(&newTime)
-			timeInterval = time.Duration(newTime) * time.Millisecond
-			fmt.Printf("[AutoSave] Settings done!\n")
-		}
-		if command == "countf" || command == "ft" {
-			fmt.Printf("[AutoSave] We fetch %d time(s)\n", fetchCount)
-		}
-		if command == "debugD" || command == "dd" {
-			fmt.Printf("[Discuss] ID?\n")
-			var discussID int
-			fmt.Scanln(&discussID)
-			ChangeDiscussToDBDiscussTemlate(discussID)
-		}
-		if command == "debugS" || command == "ds" {
-			fmt.Printf("[Discuss] ID?\n")
-			var discussID int
-			fmt.Scanln(&discussID)
-			SaveNewDiscuss(discussID)
-		}
+	if session, err := mgo.Dial("mongodb://localhost:27017"); err != nil {
+		panic(err)
+	} else {
+		defer session.Close()
+		AutoSave(session)
 	}
 }
